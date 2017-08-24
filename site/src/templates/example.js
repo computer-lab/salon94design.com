3fc21d745823d8c9cc03a5ee5de0b71b7c2d2688
Robert C Jensen <rob@computerlab.io>
2017-08-23 23:16:25 -0400

    feature: adds yaml transformer, yaml rendering example

--- site/src/templates/example.js
+++ site/src/templates/example.js
@@ -5,11 +5,16 @@ export default function Template({
   data // this prop will be injected by the GraphQL query we'll write in a bit
 }) {
   const { markdownRemark: post } = data; // data.markdownRemark holds our post data
+  const { allHeroesYaml: heroes } = data;
   return (
     <div className="blog-post-container">
       <Helmet title={`Salon 94 Design - ${post.frontmatter.title}`} />
       <div className="blog-post">
         <h1>{post.frontmatter.title}</h1>
+        Here are some of my heroes:
+        <ul>
+          {heroes.edges.map((hero) => <li>{hero.node.name}</li>)}
+        </ul>
         <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </div>
@@ -18,6 +23,13 @@ export default function Template({
 
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
+    allHeroesYaml {
+      edges {
+        node {
+          name
+        }
+      }
+    }
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
