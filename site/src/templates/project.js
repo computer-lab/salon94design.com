9c9be72ba111f7f206b532c8d7ab8a01ec3408bf
Kevin Roark Jr <kevin.e.roark@gmail.com>
2017-08-28 17:00:55 +0200

    Start ImageList component

--- site/src/templates/project.js
+++ site/src/templates/project.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import styled from 'emotion/react'
 import { PageContainer } from '../layouts/containers'
+import ImageList from '../layouts/ImageList'
 import ProjectSelector from '../layouts/ProjectSelector'
+import { pieceImagePath } from '../util'
 
 const LeftPane = styled.div`
   width: calc(100% - 360px);
@@ -12,23 +14,33 @@ const LeftPane = styled.div`
 const RightPane = styled.div`width: 360px;`
 
 export default function ProjectTemplate({ data, pathContext }) {
-  const { allProjectsYaml } = data
+  const { allProjectsYaml, allDesignersYaml } = data
   const { slug: currentProjectSlug } = pathContext
 
   const projects = allProjectsYaml.edges.map(edge => edge.node)
+  const projectSlugs = new Set(projects.map(p => p.slug))
+
+  const designers = allDesignersYaml.edges.map(edge => edge.node)
+  const pieces = designers.reduce((a, d) => {
+    const dp = d.pieces.filter(piece => {
+      return piece.projects.filter(project => projectSlugs.has(project)).length > 0
+    })
+
+    return a.concat(dp)
+  }, [])
+
+  const images = pieces
+    .reduce((a, p) => a.concat(p.images), [])
+    .map(src => ({
+      src: pieceImagePath(src),
+      text: 'Hiiiii'
+    }))
 
   return (
     <PageContainer>
       <Helmet title={`Salon 94 Design - Projects`} />
       <LeftPane>
-        <h4>Here are my projects:</h4>
-        <ul>
-          {projects.map(item =>
-            <li key={item.slug}>
-              {item.title}
-            </li>
-          )}
-        </ul>
+        <ImageList images={images} />
       </LeftPane>
       <RightPane>
         <ProjectSelector
@@ -50,6 +62,19 @@ export const pageQuery = graphql`
           description
           when
           designers
+        }
+      }
+    }
+    allDesignersYaml {
+      edges {
+        node {
+          pieces {
+            title
+            when
+            projects
+            tags
+            images
+          }
         }
       }
     }
