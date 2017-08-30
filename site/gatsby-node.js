f7d1552e536db47767f24565447f5e091eda0b11
Kevin Roark Jr <kevin.e.roark@gmail.com>
2017-08-30 11:30:37 +0200

    Create /designers pages

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,7 +1,11 @@
 const path = require('path')
 
 exports.createPages = props => {
-  return Promise.all([createBlogPosts(props), createProjects(props)])
+  return Promise.all([
+    createBlogPosts(props),
+    createProjects(props),
+    createDesigners(props)
+  ])
 }
 
 function createBlogPosts({ boundActionCreators, graphql }) {
@@ -43,7 +47,7 @@ function createBlogPosts({ boundActionCreators, graphql }) {
 function createProjects({ boundActionCreators, graphql }) {
   const { createPage } = boundActionCreators
 
-  const projectTemplate = path.resolve(`src/templates/project.js`)
+  const template = path.resolve(`src/templates/project.js`)
 
   return graphql(`{
     allProjectsYaml(
@@ -64,14 +68,52 @@ function createProjects({ boundActionCreators, graphql }) {
     // root /projects is equivalent to /projects/first_project_slug
     createPage({
       path: '/projects',
-      component: projectTemplate,
+      component: template,
       context: { slug: projects[0].slug },
     })
 
     projects.forEach(node => {
       createPage({
         path: `/projects/${node.slug}`,
-        component: projectTemplate,
+        component: template,
+        context: { slug: node.slug },
+      })
+    })
+  })
+}
+
+function createDesigners({ boundActionCreators, graphql }) {
+  const { createPage } = boundActionCreators
+
+  const template = path.resolve(`src/templates/designer.js`)
+
+  return graphql(`{
+    allDesignersYaml(
+      sort: { order: ASC, fields: [name] }
+    ) {
+      edges {
+        node {
+          slug
+          name
+        }
+      }
+    }
+  }`).then(result => {
+    if (result.errors) return Promise.reject(result.errors)
+
+    const designers = result.data.allDesignersYaml.edges.map(e => e.node)
+
+    // root /designers is equivalent to /designers/first_designer_slug
+    createPage({
+      path: '/designers',
+      component: template,
+      context: { slug: designers[0].slug },
+    })
+
+    designers.forEach(node => {
+      createPage({
+        path: `/designers/${node.slug}`,
+        component: template,
         context: { slug: node.slug },
       })
     })
