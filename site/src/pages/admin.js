8b43d1a25eaf723715fa0c662b23b8506da7f17e
Robert C Jensen <rob@computerlab.io>
2017-09-19 14:37:42 -0400

    bugfix: adds in missing admin css

--- site/src/pages/admin.js
+++ site/src/pages/admin.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const AdminPage = () =>
+  <link rel="stylesheet" href="https://unpkg.com/netlify-cms@~0.4/dist/cms.css" />
   <script src="https://unpkg.com/netlify-cms@~0.4/dist/cms.js"></script>
 
 export default AdminPage
