1490198cd7d881ae71bc5e4a51a27ee6781ccb90
Kevin Roark Jr <kevin.e.roark@gmail.com>
2017-10-04 13:08:21 -0400

    feature: homepage boolean for projects to determine whether works appear in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,21 +12,28 @@ const IndexPage = ({ data }) => {
 
   const designers = allDesignersYaml.edges.map(edge => edge.node)
   const projects = allProjectsYaml.edges.map(edge => edge.node)
-  const currentProject = projects.find(p => p.slug === 'gold-chicken-wire')
-  const filterWork = work =>
-    work.projects &&
-    work.projects.map(project => project.slug).includes(currentProject.slug)
+  const homepageProjectSlugs = projects.filter(p => p.homepage).map(p => p.slug)
+  const filterWork = work => {
+    const projects = work.projects || []
+    for (let i = 0; i < projects.length; i++) {
+      if (homepageProjectSlugs.includes(projects[i].slug)) {
+        return true
+      }
+    }
+
+    return false
+  }
 
   let images = []
   designers.forEach(designer => {
     const works = (designer.works || []).filter(filterWork)
     works.forEach(work => {
-      ;(work.images || []).forEach(image => {
+      if (work.images && work.images.length > 0) {
         images.push({
-          src: workImagePath(image.file),
+          src: workImagePath(work.images[0].file),
           unexpandedLink: workLink(designer.slug, work.slug),
         })
-      })
+      }
     })
   })
 
@@ -58,6 +65,7 @@ export const pageQuery = graphql`
         node {
           slug
           title
+          homepage
         }
       }
     }
