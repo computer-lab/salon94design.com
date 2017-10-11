458a09c43b319eb69082645882d25c9183c70f5b
Kevin Roark Jr <kevin.e.roark@gmail.com>
2017-10-11 17:35:37 -0400

    Improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'emotion/react'
+
+import { sansFontFamily } from '../layouts/emotion-base'
+
+const Container = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 12px;
+  background-color: #f9f9f9;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+
+  & h1 {
+    font-size: 72px;
+    font-family: ${sansFontFamily};
+    font-weight: 600;
+  }
+
+  & p {
+    font-size: 24px;
+    font-family: ${sansFontFamily};
+  }
+
+  & a {
+    color: inherit;
+    text-decoration: none;
+    border-bottom: 2px solid #000;
+  }
+`
 
 const NotFoundPage = () => (
-  <div>
+  <Container>
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+    <p>This page does not exist :(.</p>
+    <p>
+      Try visiting the <Link to="/">homepage</Link>.
+    </p>
+  </Container>
 )
 
 export default NotFoundPage
