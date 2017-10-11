e93032aaa35a2eb2749b158dd98ba8081d08f2ee
Robert C Jensen <rob@computerlab.io>
2017-10-11 13:52:33 -0400

    feature: transforms all the rest of the markdown fields

--- src/templates/designer.js
+++ src/templates/designer.js
@@ -115,7 +115,7 @@ const DesignerTemplate = ({ data, pathContext }) => {
       </LeftPane>
       <RightPane className="selectable">
         <Header1>{currentDesigner.name}</Header1>
-        <DesignerBio bio={currentDesigner.bio} />
+        <DesignerBio bioHtml={currentDesigner.bioHtml} />
         <DesignerProjects projects={projects} />
         <Press press={currentDesigner.press} />
         <HiddenSelector
@@ -148,7 +148,7 @@ export const pageQuery = graphql`
         node {
           slug
           name
-          bio
+          bioHtml
           press {
             title
             link
