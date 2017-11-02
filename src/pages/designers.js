a00c51328268e2d17e7cfaacc43b1907ce0ca164
Kevin Roark Jr <kevin.e.roark@gmail.com>
2017-11-02 13:11:06 -0400

    Status-based sections for designers list

--- src/pages/designers.js
+++ src/pages/designers.js
@@ -1,36 +1,42 @@
 import React, { Component } from 'react'
 import Helmet from '../components/helmet'
-import Link from 'gatsby-link'
+import Link, { navigateTo } from 'gatsby-link'
 import styled from 'emotion/react'
 
 import { PageContainer } from '../layouts/containers'
 import SectionItemList from '../layouts/SectionItemList'
 import { chooseDesignerImage, designerLink } from '../util'
 
-export default function Projects({ data }) {
-  const { allDesignersYaml } = data
+export default class Designers extends Component {
+  componentDidMount() {
+    navigateTo('/')
+  }
 
-  const designers = allDesignersYaml.edges.map(edge => edge.node)
+  render() {
+    const { allDesignersYaml } = this.props.data
 
-  const listItems = designers.map(designer => {
-    return {
-      title: designer.name,
-      image: chooseDesignerImage(designer),
-      link: designerLink(designer.slug),
-    }
-  })
+    const designers = allDesignersYaml.edges.map(edge => edge.node)
 
-  return (
-    <PageContainer>
-      <Helmet
-        title={`Salon 94 Design - Designers`}
-        description={`List of designers represented by Salon 94 Design.`}
-      />
-      <div>
-        <SectionItemList title="Designers" items={listItems} />
-      </div>
-    </PageContainer>
-  )
+    const listItems = designers.map(designer => {
+      return {
+        title: designer.name,
+        image: chooseDesignerImage(designer),
+        link: designerLink(designer.slug),
+      }
+    })
+
+    return (
+      <PageContainer>
+        <Helmet
+          title={`Salon 94 Design - Designers`}
+          description={`List of designers represented by Salon 94 Design.`}
+        />
+        <div>
+          <SectionItemList title="Designers" items={listItems} />
+        </div>
+      </PageContainer>
+    )
+  }
 }
 
 export const pageQuery = graphql`
