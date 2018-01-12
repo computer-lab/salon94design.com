b5f8a39444fb30196b84811fc0837596c4fd3712
Robert C Jensen <robert.cole.jensen@gmail.com>
2018-01-12 14:50:57 -0500

    ui: refactor works views

--- src/util/tag.js
+++ src/util/tag.js
@@ -1,44 +1,6 @@
-const categoryTagMap = {
-  furniture: new Set([
-    'furniture',
-    'beds',
-    'benches',
-    'chairs',
-    'outdoor',
-    'seating',
-    'storage',
-    'tables',
-  ]),
-  lighting: new Set(['lighting']),
-  other: new Set(['ceramics', 'fashion', 'jewelry', 'textiles', 'other']),
-}
-const categories = Object.keys(categoryTagMap)
-
-const getTagCategory = tag => {
-  for (let i = 0; i < categories.length; i++) {
-    const category = categories[i]
-    if (categoryTagMap[category].has(tag)) {
-      return category
-    }
-  }
-
-  // default to last category (other)
-  return categories[categories.length - 1]
-}
-
-const getCategoryTags = (designers, category) => {
-  const classifiedTags = getAllTags(designers)
-  const item = classifiedTags.find(item => item.category === category)
-  return item ? item.tags : []
-}
-
 const getAllTags = designers => {
-  const tagSortValue = tag => {
-    // other should always be sorted at the bottom of the list
-    return tag === 'other' ? 'zzz' : tag
-  }
-
   const tagSet = new Set()
+
   designers.forEach(designer => {
     const works = designer.works || []
     works.forEach(work => {
@@ -51,24 +13,7 @@ const getAllTags = designers => {
     })
   })
 
-  const tags = Array.from(tagSet).sort((a, b) =>
-    tagSortValue(a).localeCompare(tagSortValue(b))
-  )
-
-  const classifiedTags = {}
-  tags.forEach(tag => {
-    const category = getTagCategory(tag)
-    if (!classifiedTags[category]) {
-      classifiedTags[category] = []
-    }
-
-    classifiedTags[category].push(tag)
-  })
-
-  return categories.map(category => ({
-    category,
-    tags: classifiedTags[category] || [],
-  }))
+  return Array.from(tagSet).sort((a, b) => a.localeCompare(b))
 }
 
-module.exports = { categories, getTagCategory, getCategoryTags, getAllTags }
+module.exports = { getAllTags }
